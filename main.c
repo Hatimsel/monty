6a62e7549e04281c7f6f85fcc54ab824c4979f74
Hatimsel <selmun13@gmail.com>
2023-06-22 11:28:22 +0100

    #comments

--- main.c
+++ main.c
@@ -16,8 +16,8 @@ int main(int argc, char *argv[])
 	char *opcode;
 	instruction_t instruction_table[] = {
 		{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop},
-		{"swap", swap}, {"add", add}, {"nop", nop}, {"sub", sub},
-		{"div", div_op}, {"mul", mul}, {"mod", mod}, {"#", nop},
+		{"swap", swap}, {"add", add}, {"nop", nop}, {"#", nop},
+		{"sub", sub}, {"div", div_op}, {"mul", mul}, {"mod", mod},
 		{NULL, NULL}
 	};
 
