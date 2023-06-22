a3456762e15f3f968f8db5d9c4ac489b92958fb8
RyuzakiiL23 <elhamrani.omar23@gmail.com>
2023-06-22 21:14:57 +0100

    pchar

--- pchar.c
+++ pchar.c
@@ -16,7 +16,7 @@ void pchar(stack_t **stack, __attribute__((unused))unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 	x = (*stack)->n;
-	if (x < 47 || x > 122)
+	if (x < 32 || x > 126)
 	{
 		printf("L%d: can't pchar, value out of range", line_number);
 		exit(EXIT_FAILURE);
